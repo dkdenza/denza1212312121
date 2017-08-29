92e76e6af319ac5d6f16901e2af2d170dc5acb6d
dkdenza <31194176+dkdenza@users.noreply.github.com>
2017-08-29 08:08:18 +0700

    Add files via upload

--- ubuntu16.04.sh
+++ ubuntu16.04.sh
@@ -9,16 +9,15 @@ apt-get -y install sudo
 
 # Install Pritunl
 #!/bin/bash
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-apt-get --assume-yes update
-apt-get --assume-yes upgrade
-apt-get --assume-yes install pritunl mongodb-org
-service pritunl start
-systemctl start pritunl mongod
-systemctl enable pritunl mongod
+sudo apt-get update
+sudo apt-get --assume-yes install pritunl mongodb-org
+sudo systemctl start pritunl mongod
+sudo systemctl enable pritunl mongod
+
 
 # Install Squid
 apt-get -y install squid3
