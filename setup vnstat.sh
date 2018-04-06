34c10552c45ba228a66422f5132066b8e96e19ef
dkdenza <31194176+dkdenza@users.noreply.github.com>
2018-04-06 13:40:02 +0700

    Add files via upload

---
+++ setup vnstat.sh
@@ -0,0 +1,44 @@
+# Install Web Server
+apt-get -y install nginx php5-fpm php5-cli
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/dkdenza/denza1212312121/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by MKSSHVPN </pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/dkdenza/denza1212312121/master/vps.conf"
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+
+# Install Vnstat
+apt-get -y install vnstat
+vnstat -u -i eth0
+sudo chown -R vnstat:vnstat /var/lib/vnstat
+service vnstat restart
+
+# Install Vnstat GUI
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
+# About
+clear
+echo "Script ini :-"
+echo "-Pritunl"
+echo "-MongoDB"
+echo "-Vnstat"
+echo "-Web Server"
+echo "-Squid Proxy Port 80,8000,8080"
+echo "BY DkDenZa"
+echo "TimeZone   :  Bangkok"
+echo "Vnstat     :  http://$MYIP:81/vnstat"
